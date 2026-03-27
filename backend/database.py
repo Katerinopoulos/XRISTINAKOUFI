b127b55b23c61662b1943aa81fdd41082526a23a
Nikolas Katerinopoulos <146753591+Katerinopoulos@users.noreply.github.com>
2026-03-28 01:09:40 +0200

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,20 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# ΑΛΛΑΞΑΜΕ ΤΟ ΟΝΟΜΑ ΓΙΑ ΝΑ ΦΤΙΑΧΤΕΙ ΚΑΘΑΡΗ ΒΑΣΗ ΣΤΟ ΊΝΤΕΡΝΕΤ
-SQLALCHEMY_DATABASE_URL = "sqlite:///./live_event_app.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./production_app.db"
 
-# Δημιουργία της "μηχανής". 
-# Το check_same_thread=False χρειάζεται μόνο για την SQLite στο FastAPI.
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
-# Συνάρτηση για να παίρνουμε session της βάσης σε κάθε request
 def get_db():
     db = SessionLocal()
     try:

a0e847da2b23dd38c6d5fd67c8b497c81606df5e
Nikolas Katerinopoulos <146753591+Katerinopoulos@users.noreply.github.com>
2026-03-28 01:18:10 +0200

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./production_app.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./the_final_database.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
